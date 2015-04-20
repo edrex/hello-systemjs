fe1e749b7a2cc53fdf03fc3cbb8d2c7c7fb7d89e
Eric Drechsel <eric@pdxhub.org>
2015-04-20 15:17:03 -0700

    css plugin + bootstrap, to test css

--- app/main.js
+++ app/main.js
@@ -1,9 +1,14 @@
 "use babel";
 
 import React from 'react';
+import _ from 'bootstrap/css/bootstrap.css!';
 import Hello from 'hello';
 
 React.render(
-  <Hello name="JSPM and System.js"/>,
+  <div className="jumbotron">
+    <div className="container">
+      <Hello name="JSPM and System.js"/>
+    </div>
+  </div>,
   document.getElementById('main')
 );
