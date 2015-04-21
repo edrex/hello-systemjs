ffdda91d4affc30917b0b77d33a41909c5da91ed
Eric Drechsel <eric@pdxhub.org>
2015-04-21 15:19:36 -0700

    upgrade hello-component

--- config.js
+++ config.js
@@ -21,9 +21,13 @@ System.config({
     "core-js": "npm:core-js@0.8.3",
     "css": "github:systemjs/plugin-css@0.1.9",
     "edrex/hello-component": "github:edrex/hello-component@0.1.1",
-    "hello": "github:edrex/hello-component@0.1.1",
+    "hello": "github:edrex/hello-component@0.1.6",
     "react": "npm:react@0.13.2",
     "github:edrex/hello-component@0.1.1": {
+      "css": "github:systemjs/plugin-css@0.1.9",
+      "react": "npm:react@0.13.2"
+    },
+    "github:edrex/hello-component@0.1.6": {
       "css": "github:systemjs/plugin-css@0.1.9",
       "react": "npm:react@0.13.2"
     },
@@ -31,7 +35,7 @@ System.config({
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.1.2"
+      "buffer": "npm:buffer@3.2.0"
     },
     "github:jspm/nodelibs-events@0.1.0": {
       "events-browserify": "npm:events-browserify@0.0.1"
@@ -66,7 +70,7 @@ System.config({
       "util": "npm:util@0.10.3"
     },
     "github:systemjs/plugin-css@0.1.9": {
-      "clean-css": "npm:clean-css@3.2.1",
+      "clean-css": "npm:clean-css@3.2.2",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0"
     },
@@ -87,7 +91,25 @@ System.config({
       "ieee754": "npm:ieee754@1.1.4",
       "is-array": "npm:is-array@1.0.1"
     },
+    "npm:buffer@3.2.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.4",
+      "is-array": "npm:is-array@1.0.1"
+    },
     "npm:clean-css@3.2.1": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "commander": "npm:commander@2.8.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "http": "github:jspm/nodelibs-http@1.7.1",
+      "https": "github:jspm/nodelibs-https@0.1.0",
+      "os": "github:jspm/nodelibs-os@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "source-map": "npm:source-map@0.4.2",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:clean-css@3.2.2": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "commander": "npm:commander@2.8.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
